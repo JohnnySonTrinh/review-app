b6cd50bea71fa4557cefb8f3c7f82080f9a12fd2
Johnny Trinh <johnny.trinh@hotmail.se>
2024-05-05 13:06:02 +0000

    Create a Signup page with axios and add styles

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://star-review-api-99b8eca24b61.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
