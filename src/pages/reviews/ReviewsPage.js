65e75b86e33a4ba5adef0b2feec96aa2ebf3cefd
Johnny Trinh <johnny.trinh@hotmail.se>
2024-05-08 14:34:59 +0000

    Add Search bar

--- src/pages/reviews/ReviewsPage.js
+++ src/pages/reviews/ReviewsPage.js
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import Review from './Review';
-import Asset from '../../components/Asset';
 
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 
-import NoResults from '../../assets/no-results.png';
+import Review from './Review';
+import Asset from '../../components/Asset';
 
 import appStyles from '../../App.module.css';
 import styles from '../../styles/ReviewsPage.module.css';
 import { useLocation } from 'react-router';
 import { axiosReq } from '../../api/axiosDefaults';
 
-function ReviewsPage(message, filter = '') {
+import NoResults from '../../assets/no-results.png';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { fetchMoreData } from '../../utils/utils';
+
+function ReviewsPage({ message, filter = '' }) {
   const [reviews, setReviews] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const { data } = await axiosReq.get(`/reviews/?${filter}`);
+        const { data } = await axiosReq.get(
+          `/reviews/?${filter}search=${query}`
+        );
         setReviews(data);
         setHasLoaded(true);
       } catch (err) {
@@ -31,19 +38,45 @@ function ReviewsPage(message, filter = '') {
     };
 
     setHasLoaded(false);
-    fetchReviews();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchReviews();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
 
   return (
     <Row className='h-100'>
       <Col className='py-2 p-0 p-lg-2 mt-5' lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type='text'
+            className='mr-sm-2'
+            placeholder='Search'
+          />
+        </Form>
+
         {hasLoaded ? (
           <>
             {reviews.results.length ? (
-              reviews.results.map((review) => (
-                <Review key={review.id} {...review} setReviews={setReviews} />
-              ))
+              <InfiniteScroll
+                children={reviews.results.map((review) => (
+                  <Review key={review.id} {...review} setReviews={setReviews} />
+                ))}
+                dataLength={reviews.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!reviews.next}
+                next={() => fetchMoreData(reviews, setReviews)}
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
@@ -56,7 +89,7 @@ function ReviewsPage(message, filter = '') {
           </Container>
         )}
       </Col>
-      <Col md={4} className='d-none d-lg-block p-0 p-lg-2 mt-5'>
+      <Col md={4} className='d-none d-lg-block p-0 p-lg-2'>
         <p>Popular profiles for desktop</p>
       </Col>
     </Row>
