5847a2eb6f87dd46dfaf4ac5e198dee347845b52
Johnny Trinh <johnny.trinh@hotmail.se>
2024-05-08 13:40:24 +0000

    Add Review list

--- src/App.js
+++ src/App.js
@@ -8,18 +8,53 @@ import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import ReviewCreateForm from './pages/reviews/ReviewCreateForm';
 import ReviewPage from './pages/reviews/ReviewPage';
+import ReviewsPage from './pages/reviews/ReviewsPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || '';
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path='/' render={() => <h1>Home Page</h1>} />
+          <Route
+            exact
+            path='/'
+            render={() => (
+              <ReviewsPage message='No results found. Adjust the search keyword' />
+            )}
+          />
+          <Route
+            exact
+            path='/feed'
+            render={() => (
+              <ReviewsPage
+                message='No results found. Adjust the search keyword or follow a user'
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/liked'
+            render={() => (
+              <ReviewsPage
+                message='No results found. Adjust the search keyword or like a review'
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
           <Route exact path='/signin' render={() => <SignInForm />} />
           <Route exact path='/signup' render={() => <SignUpForm />} />
           <Route exact path='/posts/create' render={() => <PostCreateForm />} />
-          <Route exact path='/reviews/create' render={() => <ReviewCreateForm />} />
+          <Route
+            exact
+            path='/reviews/create'
+            render={() => <ReviewCreateForm />}
+          />
           <Route exact path='/reviews/:id' render={() => <ReviewPage />} />
           <Route render={() => <h1>404 Page not found!</h1>} />
         </Switch>
