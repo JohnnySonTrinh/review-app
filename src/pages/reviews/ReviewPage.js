ef866d4b2367c2edc8558ee84aa042339f6f0f2b
Johnny Trinh <johnny.trinh@hotmail.se>
2024-05-09 01:08:20 +0000

    Create Note component and refactor styles

--- src/pages/reviews/ReviewPage.js
+++ src/pages/reviews/ReviewPage.js
@@ -8,6 +8,7 @@ import appStyles from '../../App.module.css';
 import { useParams } from 'react-router';
 import { axiosReq } from '../../api/axiosDefaults';
 import Review from './Review';
+import Note from '../notes/Note';
 
 import NoteCreateForm from '../notes/NoteCreateForm';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
@@ -21,19 +22,28 @@ function ReviewPage() {
   const [notes, setNotes] = useState({ results: [] });
 
   useEffect(() => {
-    const handleMount = async () => {
+    console.log(`Component mounting or updating for review ID: ${id}`);
+
+    const fetchData = async () => {
       try {
-        const [{ data: review }] = await Promise.all([
+        const [{ data: review }, { data: notes }] = await Promise.all([
           axiosReq.get(`/reviews/${id}`),
+          axiosReq.get(`/notes/?review=${id}`),
         ]);
         setReview({ results: [review] });
-        console.log(review);
+        setNotes(notes);
+        console.log(notes);
+        console.log(`Notes fetched for review ID ${id}:`, notes);
       } catch (err) {
-        console.log(err);
+        console.error(`Error fetching data for review ID ${id}:`, err);
       }
     };
 
-    handleMount();
+    fetchData();
+
+    return () => {
+      console.log(`Cleaning up for review ID ${id}`);
+    };
   }, [id]);
 
   return (
@@ -41,7 +51,7 @@ function ReviewPage() {
       <Col className='py-2 p-0 p-lg-2' lg={8}>
         <p>Popular profiles for mobile</p>
         <Review {...review.results[0]} setReviews={setReview} reviewPage />
-        <Container className={appStyles.Content}>
+        <Container className={`${appStyles.Content}`}>
           {currentUser ? (
             <NoteCreateForm
               profile_id={currentUser.profile_id}
@@ -53,6 +63,15 @@ function ReviewPage() {
           ) : notes.results.length ? (
             'Notes'
           ) : null}
+          {notes.results.length ? (
+            notes.results
+              .filter((note) => note.review === review.results[0].id)
+              .map((note) => <Note key={note.id} {...note} />)
+          ) : currentUser ? (
+            <span>No notes yet, be the first to note!</span>
+          ) : (
+            <span>No notes... yet</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className='d-none d-lg-block p-0 p-lg-2'>
