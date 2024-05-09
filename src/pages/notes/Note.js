afd70a16a9eb8201f147e8801f95043201e75116
Johnny Trinh <johnny.trinh@hotmail.se>
2024-05-09 02:41:43 +0000

    Add Edit Note Form

--- src/pages/notes/Note.js
+++ src/pages/notes/Note.js
@@ -1,36 +1,48 @@
-import React from 'react'
-import { Media } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Avatar from "../../components/Avatar";
+import React, { useState } from "react";
+import { Media } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Avatar from '../../components/Avatar';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import NoteEditForm from './NoteEditForm';
+
 import styles from '../../styles/Note.module.css';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { MoreDropdown } from '../../components/MoreDropdown';
 import { axiosRes } from '../../api/axiosDefaults';
 
 const Note = (props) => {
-  const { profile_id, profile_image, owner, updated_on, content, id, setReview, setNotes } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_on,
+    content,
+    id,
+    setReview,
+    setNotes,
+  } = props;
 
+  const [showEditForm, setShowEditForm] = useState(false);
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
   const handleDelete = async () => {
     try {
-      await axiosRes.delete(`/notes/${id}/`)
-      setReview(prevReview => ({
-        results: [{
-          ...prevReview.results[0],
-          notes_count: prevReview.results[0].notes_count - 1
-        }]
-      }))
+      await axiosRes.delete(`/notes/${id}/`);
+      setReview((prevReview) => ({
+        results: [
+          {
+            ...prevReview.results[0],
+            notes_count: prevReview.results[0].notes_count - 1,
+          },
+        ],
+      }));
 
-      setNotes(prevNotes => ({
+      setNotes((prevNotes) => ({
         ...prevNotes,
-        results: prevNotes.results.filter(note => note.id !== id),
-      }))
-    } catch(err){
-
-    }
-  }
+        results: prevNotes.results.filter((note) => note.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -39,17 +51,31 @@ const Note = (props) => {
         <Link to={`/profiles/${profile_id}`}>
           <Avatar src={profile_image} />
         </Link>
-        <Media.Body className="align-self-center ml-2">
+        <Media.Body className='align-self-center ml-2'>
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_on}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <NoteEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setNotes={setNotes}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
         {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete}/>
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
     </div>
   );
 };
 
-export default Note
+export default Note;
